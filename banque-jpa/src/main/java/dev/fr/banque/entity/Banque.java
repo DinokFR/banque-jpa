704ec4ad4f6906209786313dd56713b6d80eda01
Fromage <florian.marcot@hotmail.fr>
2024-11-07 14:21:44 +0100

    Tp4 wip

---
+++ banque-jpa/src/main/java/dev/fr/banque/entity/Banque.java
@@ -0,0 +1,12 @@
+package dev.fr.banque.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Banque")
+public class Banque {
+
+}
